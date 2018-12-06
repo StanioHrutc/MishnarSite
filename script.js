8e58e07d87b085f7dbafe3b5a3fd0277defe81f5
hrutc_stas <grucushun90@gmail.com>
2018-12-06 17:58:32 +0200

    Final commit.Project is over

---
+++ script.js
@@ -0,0 +1,8 @@
+function goToFacebook() {
+	document.location.href = "https://www.facebook.com/profile.php?id=100008016316667";	
+}
+
+
+function goToTumblr() {
+	document.location.href = "https://scarlet--holmes.tumblr.com/";
+}
